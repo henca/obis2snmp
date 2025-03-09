09935c8487c29eac383cac9c317a75d355603ffb
Henrik Carlqvist <hc1245@poolhem.se>
2025-03-09 18:32:07 +0100

    Putting obis files in a directory which also has the hostname, allowing to have obis data from multiple hosts.

--- contrib/mrtg/obis_create_page.sh
+++ contrib/mrtg/obis_create_page.sh
@@ -11,15 +11,22 @@ if [ $# -ne 1 ]; then
   echo "mrtg files. The host must have snmpd running and the obis2snmpd"
   echo "daemon configured running."
 else
-mkdir -p `dirname $0`/obis
-cd `dirname $0`/obis
+mkdir -p `dirname $0`/${1}_obis
+cd `dirname $0`/${1}_obis
+
+# A line like this might be useful if you have some kind of automait
+# directory listing on your web server for your mrtg pages which expects
+# a folder icon. Most people do not have such automatic listings, but at
+# least the link will not do much harm.
 ln -sf ../meter.png folder.png
+
+
 cd -
 
-/opt/bin/cfgmaker --global "WorkDir: `dirname $(pwd)/$0`/obis" \
+/opt/bin/cfgmaker --global "WorkDir: `dirname $(pwd)/$0`/${1}_obis" \
                                --global 'Options[_]: bits,growright' \
                                --host-template=obis2snmp.htp \
 			       --nointerfaces \
-                               public@${1}:::::2 > `dirname $0`/obis/mrtg.cfg
+                               public@${1}:::::2 > `dirname $0`/${1}_obis/mrtg.cfg
 fi
 
