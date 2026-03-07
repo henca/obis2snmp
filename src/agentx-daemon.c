9ac8e88990fbdb9cbc5c7a7a0c2d761175bc9371
Henrik Carlqvist <hc1245@poolhem.se>
2026-03-07 17:37:38 +0100

    Preparing for future work on version 1.3

--- src/agentx-daemon.c
+++ src/agentx-daemon.c
@@ -42,7 +42,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "obis2snmp.h"
 #include <net-snmp/agent/util_funcs.h>
 
-#define VERSION_STRING "1.2"
+#define VERSION_STRING "1.3beta"
 
 struct driver_data {
    void *dlhandle;
